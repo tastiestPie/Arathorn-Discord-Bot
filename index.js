c0c4510151dc3c4372777e3c7a5ddfb0d0fa60c7
Gabriel Que <gabeque11@gmail.com>
2025-04-23 16:27:33 -0400

    Updated Init

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Login to Discord with your client's token
+// Log in to Discord with your client's token
 client.login(token);
-
-//to start bot, type "node index.js" into the cmd
-//access the cmd by hitting Ctrl `
-//to instantly cd into the folder, use \Users\gabeq\Desktop\Arathorn-Discord-Bot as the pathway (only for my computer obv)
